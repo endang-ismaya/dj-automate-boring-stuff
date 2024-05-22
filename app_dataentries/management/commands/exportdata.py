779df43a81c0289ec1f845941e397e3a8993a8b9
endangismaya <endang.ismaya@gmail.com>
2024-05-22 16:34:28 +0700

    Section 5: Export Data custom command completed

---
+++ app_dataentries/management/commands/exportdata.py
@@ -0,0 +1,52 @@
+import csv
+
+from django.apps import apps
+from django.core.management.base import BaseCommand
+
+from app_dataentries.utils import generate_csv_file
+
+
+# propsed command = python manage.py exportdata model_name
+class Command(BaseCommand):
+    help = "Export data from the database to a CSV file"
+
+    def add_arguments(self, parser):
+        parser.add_argument("model_name", type=str, help="Model name")
+
+    def handle(self, *args, **kwargs):
+        model_name = kwargs["model_name"].capitalize()
+
+        # search through all the installed apps for the model
+        model = None
+        for app_config in apps.get_app_configs():
+            try:
+                model = apps.get_model(app_config.label, model_name)
+                break  # stop executing once the model is found
+            except LookupError:
+                pass
+
+        if not model:
+            self.stderr.write(f"Model {model_name} cound not found")
+            return
+
+        # fetch the data from the database
+        data = model.objects.all()
+
+        # generate csv file path
+        file_path = generate_csv_file(model_name)
+
+        # open the csv file and write the data
+        with open(file_path, "w", newline="") as file:
+            writer = csv.writer(file)
+
+            # write the CSV header
+            # we want to print the field names of the model that we are trying to export
+            writer.writerow([field.name for field in model._meta.fields])
+
+            # write data rows
+            for dt in data:
+                writer.writerow(
+                    [getattr(dt, field.name) for field in model._meta.fields]
+                )
+
+        self.stdout.write(self.style.SUCCESS("Data exported successfully!"))
--- app_dataentries/utils.py
+++ app_dataentries/utils.py
@@ -1,8 +1,29 @@
 import csv
+import datetime
+import os
 
 from django.apps import apps
+from django.conf import settings
 from django.core.management.base import CommandError
 from django.db import DataError
+
+
+def get_all_custom_models():
+    default_models = [
+        "ContentType",
+        "Session",
+        "LogEntry",
+        "Group",
+        "Permission",
+        "User",
+        "Upload",
+    ]
+    # try to get all the apps
+    custom_models = []
+    for model in apps.get_models():
+        if model.__name__ not in default_models:
+            custom_models.append(model.__name__)
+    return custom_models
 
 
 def check_csv_errors(file_path, model_name):
@@ -36,3 +57,79 @@ def check_csv_errors(file_path, model_name):
         raise e
 
     return model
+
+
+# def send_email_notification(
+#     mail_subject, message, to_email, attachment=None, email_id=None
+# ):
+#     try:
+#         from_email = settings.DEFAULT_FROM_EMAIL
+#         for recipient_email in to_email:
+#             # Create EmailTracking record
+#             new_message = message
+#             if email_id:
+#                 email = Email.objects.get(pk=email_id)
+#                 subscriber = Subscriber.objects.get(
+#                     email_list=email.email_list, email_address=recipient_email
+#                 )
+#                 timestamp = str(time.time())
+#                 data_to_hash = f"{recipient_email}{timestamp}"
+#                 unique_id = hashlib.sha256(data_to_hash.encode()).hexdigest()
+#                 email_tracking = EmailTracking.objects.create(
+#                     email=email,
+#                     subscriber=subscriber,
+#                     unique_id=unique_id,
+#                 )
+
+#                 base_url = settings.BASE_URL
+#                 # Generate the tracking pixel url
+#                 click_tracking_url = f"{base_url}/emails/track/click/{unique_id}"
+#                 open_tracking_url = f"{base_url}/emails/track/open/{unique_id}"
+
+#                 # Search for the links in the email body
+#                 soup = BeautifulSoup(message, "html.parser")
+#                 urls = [a["href"] for a in soup.find_all("a", href=True)]
+#                 print("urls=>", urls)
+
+#                 # If there are links or urls in the email body, inject our click tracking url to that original link
+#                 if urls:
+#                     for url in urls:
+#                         # make the final tracking url
+#                         tracking_url = f"{click_tracking_url}?url={url}"
+#                         new_message = new_message.replace(f"{url}", f"{tracking_url}")
+#                 else:
+#                     print("No URLs found in the email content")
+
+#                 # Create the email content with tracking pixel image
+#                 open_tracking_img = (
+#                     f"<img src='{open_tracking_url}' width='1' height='1'>"
+#                 )
+#                 new_message += open_tracking_img
+
+#             mail = EmailMessage(
+#                 mail_subject, new_message, from_email, to=[recipient_email]
+#             )
+#             if attachment is not None:
+#                 mail.attach_file(attachment)
+
+#             mail.content_subtype = "html"
+#             mail.send()
+#         # Store the total sent emails inside the Sent model
+#         if email_id:
+#             sent = Sent()
+#             sent.email = email
+#             sent.total_sent = email.email_list.count_emails()
+#             sent.save()
+#     except Exception as e:
+#         raise e
+
+
+def generate_csv_file(model_name):
+    # generate the timestamp of current date and time
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    # define the csv file name/path
+
+    export_dir = "exported_data"
+    file_name = f"exported_{model_name}_data_{timestamp}.csv"
+    file_path = os.path.join(settings.MEDIA_ROOT, export_dir, file_name)
+    return file_path
