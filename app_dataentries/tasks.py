631be3a79ab4fe8c016060b339e7da61239da03b
endangismaya <endang.ismaya@gmail.com>
2024-05-23 10:02:44 +0700

    Section 7: testing upload thru GUI

--- app_dataentries/tasks.py
+++ app_dataentries/tasks.py
@@ -1,10 +1,8 @@
 import time
 
+from django.core.management import call_command
+
 from _prj.celery import app
-
-# from django.core.management import call_command
-
-
 # from .utils import generate_csv_file, send_email_notification
 
 
@@ -20,18 +18,18 @@ def celery_test_task():
     return "Task executed successfully"
 
 
-# @app.task
-# def import_data_task(file_path, model_name):
-#     try:
-#         call_command("importdata", file_path, model_name)
-#     except Exception as e:
-#         raise e
-#     # notify the user by email
-#     mail_subject = "Import Data Completed"
-#     message = "Your data import has been successful"
-#     to_email = settings.DEFAULT_TO_EMAIL
-#     send_email_notification(mail_subject, message, [to_email])
-#     return "Data imported successfully."
+@app.task
+def import_data_task(file_path, model_name):
+    try:
+        call_command("importdata", file_path, model_name)
+    except Exception as e:
+        raise e
+    # notify the user by email
+    # mail_subject = "Import Data Completed"
+    # message = "Your data import has been successful"
+    # to_email = settings.DEFAULT_TO_EMAIL
+    # send_email_notification(mail_subject, message, [to_email])
+    return "Data imported successfully."
 
 
 # @app.task
